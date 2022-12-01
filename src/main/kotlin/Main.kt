3c0dc8b30923c35230187575f328c0bb6f877c71
Fabian Jäger <me@fabianjaeger.de>
2022-12-01 17:21:09 +0100

    final 5V

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 
 fun main() {
 
-    println("Hallo!\nWelche Aufgabe soll gelöst werden (3A, 5A)?\n")
-    when(readLine()?.uppercase()) {
-        "3A" -> {
-            MonoalphabetischeSubstitution().solve()
+    println("Hallo!\nWelche Aufgabe soll gelöst werden (3A, 5A)?")
+
+    do {
+        val input = readLine()?.uppercase()
+        when(input) {
+            "3A" -> {
+                MonoalphabetischeSubstitution().solve()
+            }
+            "5A" -> {
+                RsaCrack().solve()
+            }
+            else -> {
+                println("$input not found, try again!")
+            }
         }
-        "5A" -> {
-            RsaCrack().solve()
-        }
-        else -> {
-            return
-        }
-    }
+    } while (input != "")
 }
